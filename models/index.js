5c0b6a25d1913b79d5e2f3d125ebc10a8c91ceb9
dennischen123 <dennischen123@gmail.com>
2020-04-10 11:23:10 -0700

    Database setup

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const DB_URL = process.env.MONGODB_URI;
+
+mongoose.connect(DB_URL, {
+    useNewURLParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+module.exports = {
+    User: require('./User.js'),
+    Product: require('./Product'),
+}
